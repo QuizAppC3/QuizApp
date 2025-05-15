c3066484cda7306b075c6ae752f89ad2d104f8d9
root <root@Computer>
2025-05-15 09:28:13 +0200

    view application layout erstellt und grundlegende Welcome Page für root url erstellt/festgelegt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get "welcome/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#index' # anfragen auf root url werden auf welcome page geleitet
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
