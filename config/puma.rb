62aa2d4b4153dfd2ee182acf42cca70de8b92e00
leon-26 <nahmleon@gmail.com>
2025-05-16 10:14:04 +0200

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -29,8 +29,8 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 # Azure expects the app to listen on 0.0.0.0 and port 8000
-port ENV.fetch("PORT", 8000)
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 8000)}"
+port ENV.fetch("PORT", 3000)
+
 
 
 # Allow puma to be restarted by `bin/rails restart` command.
