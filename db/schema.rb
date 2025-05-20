fbd3785c064af6c57e5d599ff761826d3c114a1a
sweise <steffen.weise@akquinet.de>
2025-05-20 12:20:53 +0200

    foreign key fix?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_20_083703) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_20_101718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_20_083703) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
