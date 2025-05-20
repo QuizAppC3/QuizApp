6640c46f9738b51f888845f3467fcaa751b0f072
craether <christoph.raether@akquinet.de>
2025-05-20 11:33:45 +0200

    haetu

--- test/factories/questions.rb
+++ test/factories/questions.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :question do
-    
+    # Annahme: 'frage' ist der Name der Spalte für die Frage
+    frage { Faker::Lorem.question }
+
+    # Die vier Antwortmöglichkeiten
+    antwort1 { Faker::Lorem.word }
+    antwort2 { Faker::Lorem.word }
+    antwort3 { Faker::Lorem.word }
+    antwort4 { Faker::Lorem.word }
+
+    # Die korrekte Antwort
+    # Wir stellen sicher, dass 'korrekt' einer der 'antwort'-Werte ist
+    korrekt { |q| [q.antwort1, q.antwort2, q.antwort3, q.antwort4].sample }
+
+    # Die Kategorie, die du bereits verwendet hast
+    kategorie { Faker::Educator.subject }
   end
 end
